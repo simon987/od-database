ab25d821c66fdedc2fd7329b91e94563c5b4a652
Simon <fortier.simon@hotmail.com>
2018-06-07 11:35:56 -0400

    Export job no longer let the user download incomplete archives

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sqlite3 -header -csv db.sqlite3 < export.sql > static/out.csv
-echo "Exported $(wc -l < static/out.csv) files"
-rm static/out.csv.xz
-xz static/out.csv
+sqlite3 -header -csv db.sqlite3 < export.sql > out.csv.temp
+echo "Exported $(wc -l < out.csv.temp) files"
+xz out.csv.temp
+mv out.csv.temp.xz static/out.csv.xz
 echo "Compressed to $(stat --printf="%s" static/out.csv.xz) bytes"
