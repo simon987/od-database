c5deafbea5c821b4e765468a64e789e2ea1dd63f
Simon <fortier.simon@hotmail.com>
2018-06-20 13:34:41 -0400

    Should fix some odd http listings

--- crawl_server/remote_http.py
+++ crawl_server/remote_http.py
@@ -180,7 +180,8 @@ class HttpDirectory(RemoteDirectory):
 
     @staticmethod
     def _should_ignore(base_url, link: Anchor):
-        if link.text == "../" or link.href == "../" or link.href.endswith(HttpDirectory.BLACK_LIST):
+        if link.text == "../" or link.href == "../" or link.href == "./" \
+                or link.href.endswith(HttpDirectory.BLACK_LIST):
             return True
 
         # Ignore external links
