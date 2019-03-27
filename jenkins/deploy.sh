b170f9bfd8fdb26a25cc849f6c7c66f1801d73cb
simon987 <fortier.simon@protonmail.com>
2019-03-27 17:40:59 -0400

    Redirect stderr to file

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,6 +5,6 @@ export ODDBROOT="od-database"
 screen -S oddb_web -X quit
 killall -9 uwsgi
 echo "starting oddb_web"
-screen -S oddb_web -d -m bash -c "cd ${ODDBROOT} && uwsgi od-database.ini"
+screen -S oddb_web -d -m bash -c "cd ${ODDBROOT} && uwsgi od-database.ini 2> stderr.txt"
 sleep 1
 screen -list
