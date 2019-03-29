f35cc9dd5b639d7666da228b7b102bc47bc2ac04
simon987 <fortier.simon@protonmail.com>
2019-03-28 20:54:27 -0400

    jenkins tweaks

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,6 +4,9 @@ export ODDBROOT="od-database"
 
 screen -S oddb_web -X quit
 killall -9 uwsgi
+
+sleep 30
+
 echo "starting oddb_web"
 screen -S oddb_web -d -m bash -c "cd ${ODDBROOT} && uwsgi od-database.ini 2> stderr.txt"
 sleep 1
